159e716b75daad514c9e69cac494d32f5a833024
arabinelli <andrea.rabinelli@gmail.com>
2020-04-17 21:43:54 +0200

    style(connectors/slack): changed notify arguments name for improved interpretability

--- groundhog/connectors/slack.py
+++ groundhog/connectors/slack.py
@@ -18,13 +18,13 @@ class SlackConnector():
         self.message_template = TemplateErrorNotification(channel)
         
 
-    def notify(self,monitored_app_name,func,error,args,kwargs):
+    def notify(self,monitored_app_name,func,error,func_args,func_kwargs):
         """Sends notification to slack channel"""
         payload = self.message_template\
                         .get_notification_payload(monitored_app_name = monitored_app_name,
                                                   func=func,
                                                   e = error,
-                                                  function_args=args,
-                                                  function_kwargs=kwargs)
+                                                  function_args=func_args,
+                                                  function_kwargs=func_kwargs)
 
         self.slack_web_client.chat_postMessage(**payload)
