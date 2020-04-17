eda47edebd9fa4a0fc3b344242d67d89851e6eef
arabinelli <andrea.rabinelli@gmail.com>
2020-04-17 21:43:00 +0200

    docs(groundhog): documented the register_connector method

--- groundhog/groundhog.py
+++ groundhog/groundhog.py
@@ -13,15 +13,25 @@ class Groundhog():
             try:
                 return func(*args, **kwargs)
             except BaseException as e:
-                self.call_out(func,e,*args,**kwargs)
+                self._call_out(func,e,*args,**kwargs)
                 raise e
         return wrapper
     
-    def call_out(self,func,e,*args,**kwargs):
+    def _call_out(self,func,e,*args,**kwargs):
         for connector in self.connectors:
             connector.notify(self.app_name,func,e,args,kwargs)
     
     def register_connector(self,Connector,*connector_args,**connector_kwargs):
+        """
+        Registers a new connector.
+
+        ARGUMENTS:
+        Connector (class): A connector is a python class with a notify method 
+                           taking as arguments: monitored_app_name, func, error, 
+                           func_args and func_kwargs
+        *connector_args: any arguments needed to initialize the connector
+        **connector_kwargs: any keyword arguments needed to initialize the connector
+        """
         connector_instance = Connector(*connector_args,**connector_kwargs)
         self.connectors.append(connector_instance)
 
@@ -39,7 +49,7 @@ if __name__ == "__main__":
     alan.register_connector(SlackConnector,SLACK_BOT_TOKEN,CHANNEL)
 
     # assign the sentinel to watch this function
-    @adam.watch
+    @alan.watch
     def failable_func(n):
         """Just a function that could easily fail with the wrong input"""
         math.sqrt(n)
@@ -49,6 +59,3 @@ if __name__ == "__main__":
 
     # doesn't look too good...
     failable_func(-4)
-
-
-    #ciaone(-4)
